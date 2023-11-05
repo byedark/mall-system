509d602fd3096d376cc115d6c88ebd4103ba353d
xiatian <xiatiancode@163.com>
2023-11-05 18:33:25 +0800

    项目初始化

---
+++ mall-order/src/test/java/com/xiatian/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiatian.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
